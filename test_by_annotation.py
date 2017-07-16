76f1150ef82685b37dac1aed66b384414b904b01
Jendrik Seipp <jendrikseipp@gmail.com>
2017-07-16 15:19:28 +0200

    simplify test script

--- test_by_annotation.py
+++ test_by_annotation.py
@@ -1,36 +1,38 @@
 from __future__ import print_function
 
 import ast
+from collections import defaultdict
 import sys
+
 from vulture import lines
 from vulture.core import ENCODING_REGEX
 
 
-def annotate(source, tree):
+def annotate(tree):
+    line_to_sizes = defaultdict(list)
     for entry in ast.walk(tree):
-        if isinstance(entry, (ast.ClassDef, ast.FunctionDef)):
+        # Modules don't have a lineno attribute.
+        if hasattr(entry, 'lineno'):
             size = lines.count_lines(entry)
-            last_line = entry.lineno + size - 1
-            if isinstance(source, str):
-                source = source.split("\n")
-            for i, line in enumerate(source, 1):
-                # Print the trailing code as well to make sure
-                # we are not missing something.
-                if i in range(entry.lineno, last_line + 4):
-                    if i == entry.lineno:
-                        print("%3s %s" % (size, line))
-                    else:
-                        print("%3s %s" % (' ', line))
-            print("\n\n")
+            if size > 1:
+                line_to_sizes[entry.lineno].append(size)
+    return line_to_sizes
+
+
+def print_annotated(source, line_to_sizes):
+    for i, line in enumerate(source.splitlines(), start=1):
+        sizes = '+'.join(str(size) for size in line_to_sizes.get(i, []))
+        print("%8s %s" % (sizes, line))
 
 
 def main():
     filename = sys.argv[1]
     with open(filename) as f:
         source = f.read()
-        source = ENCODING_REGEX.sub("", source, count=1)
-        tree = ast.parse(source)
-        annotate(source, tree)
+    source = ENCODING_REGEX.sub("", source, count=1)
+    tree = ast.parse(source)
+    line_to_sizes = annotate(tree)
+    print_annotated(source, line_to_sizes)
 
 
 if __name__ == '__main__':
